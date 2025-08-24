a79db2e6f0cc0c312911913ab0c6a392960a708b
notURandomDev <1250901577@qq.com>
2025-08-25 01:22:23 +0800

    feat：初始化音乐界面

---
+++ app/music/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function MusicPage() {
+  return (
+    <div>
+      <h1 className={title()}>Stay Tuned...</h1>
+    </div>
+  );
+}
