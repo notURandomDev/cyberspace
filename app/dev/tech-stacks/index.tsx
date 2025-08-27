3dcddd8b2875855805ba96268ced7b58fed379ba
notURandomDev <1250901577@qq.com>
2025-08-28 00:58:40 +0800

    fix：优化技术栈栏目在手机屏幕上的布局

--- app/dev/tech-stacks/index.tsx
+++ app/dev/tech-stacks/index.tsx
@@ -4,6 +4,7 @@ import { Tabs, Tab } from "@heroui/tabs";
 import { TECH_STACKS } from "./data";
 import { Progress } from "@heroui/progress";
 import { useTheme } from "next-themes";
+import { useIsSmallScreen } from "@/hooks/use-window-size";
 
 const TECH_STACK_TABS = [
   {
@@ -18,15 +19,16 @@ const TECH_STACK_TABS = [
 
 const TechStacksSection = () => {
   const { theme } = useTheme();
-
+  const isSmallScreen = useIsSmallScreen(768);
+  const isVertical = !isSmallScreen;
   return (
-    <Tabs isVertical>
+    <Tabs variant="light" isVertical={isVertical}>
       {TECH_STACK_TABS.map((techStackTab) => (
         <Tab className="flex flex-col w-full gap-3" title={techStackTab.name}>
           <Card>
             <CardBody>{`这些是我掌握的${techStackTab.name}`}</CardBody>
           </Card>
-          <div className="grid grid-cols-4 gap-3">
+          <div className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-3">
             {TECH_STACKS.filter(
               (techStack) => techStack.type === techStackTab.type
             ).map((techStack) => {
@@ -38,13 +40,15 @@ const TechStacksSection = () => {
                   <techStack.icon></techStack.icon>
                 );
               return (
-                <Card key={name} className="w-full p-2">
+                <Card key={name} className="w-full p-2 sm:p-3">
                   <CardHeader className="flex flex-col gap-4">
-                    <div className="flex w-full gap-3 grow">
-                      {TechIcon}
-                      <p className="text-base font-semibold">{name}</p>
+                    <div className="flex w-full gap-2 sm:gap-3 grow items-center">
+                      <div className="flex-shrink-0">{TechIcon}</div>
+                      <p className="text-sm sm:text-base font-semibold">
+                        {name}
+                      </p>
                     </div>
-                    <div className="w-full ">
+                    <div className="w-full">
                       <Progress
                         label={desc}
                         size="sm"
@@ -54,6 +58,10 @@ const TechStacksSection = () => {
                         minValue={0}
                         maxValue={5}
                         color="success"
+                        classNames={{
+                          label: "text-xs sm:text-sm",
+                          value: "text-xs sm:text-sm",
+                        }}
                       />
                     </div>
                   </CardHeader>
